5191467eb95608ceed2fc47bf95b13c904f10c4c
MurrTuck <murrtuck@gmail.com>
2020-05-19 13:15:32 -0600

    Mini Complete

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const ctrl = require('./controller')
+const SERVER_PORT = 4000
+
+app.use(express.json())
+
+app.get('/api/books', ctrl.getAllBooks)
+app.post('/api/books', ctrl.createBook)
+app.put('/api/books/:book_id', ctrl.editBook)
+app.delete('/api/books/:book_id', ctrl.deleteBook)
+
+
+app.listen(SERVER_PORT, () => console.log(`Sever listenting on port ${SERVER_PORT}`))
+
